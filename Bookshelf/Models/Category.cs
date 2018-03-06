7e95860dc015a3936950193f2fa25cef3bf0d466
AHoobler <ashley.hoobler@gmail.com>
2018-03-06 15:15:42 -0500

    added controllers and styled

---
+++ Bookshelf/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Bookshelf.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+
+        public string Name { get; set; }
+
+        //navigation property. one-to-many
+        public virtual ICollection<Book>Books { get; set; }
+    }
+}
